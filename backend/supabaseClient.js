ede1335677ca8f854b8b37a0dfe8c52d09e06b99
Kurooo23 <142067591+Kurooo23@users.noreply.github.com>
2025-12-24 20:46:38 +0800

    updated

--- backend/supabaseClient.js
+++ backend/supabaseClient.js
@@ -1,4 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
+import process from 'node:process'
+import dotenv from 'dotenv'
+dotenv.config()
 const supabase = createClient(
   process.env.SUPABASE_URL,
   process.env.SUPABASE_SERVICE_ROLE_KEY,
